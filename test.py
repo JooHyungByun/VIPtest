c578ed09c199984388ec4beb53a996a0dd9b3c11
JooHyungByun <quswngud3@kw.ac.kr>
2020-09-21 22:13:17 +0900

    3

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2
-# IMREAD_COLOR
-img = cv2.imread('lenna.png', 1)
+# IMREAD_GRAYSCALE
+img = cv2.imread('lenna.png', 0)
 cv2.imshow('lenna', img):
 cv2.waitKey(0)
 cv2.destroyAllWindows()
