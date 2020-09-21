9a4adc60271ab85b36caeab46280ccff91fadad2
JooHyungByun <quswngud3@kw.ac.kr>
2020-09-21 21:21:48 +0900

    add esc key and 's'(save) key

--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import cv2
 # IMREAD_COLOR
 img = cv2.imread('lenna.png', 1)
 cv2.imshow('lenna', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+k = cv2.waitKey(0)
+if k == 27: #esc key
+    cv2.destroyAllWindows()
+elif k == ord('s'): #'s' key
+    cv2.imwrite('lenna2.png', 1)
+    cv2.destroyAllWindows()
+
+
