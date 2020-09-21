98fa580634ee67cfc2e797d94068d85dd6bcc44a
JooHyungByun <quswngud3@kw.ac.kr>
2020-09-21 21:32:04 +0900

    Revert "add esc key and 's'(save) key"
    This reverts commit 9a4adc60271ab85b36caeab46280ccff91fadad2.

--- test.py
+++ test.py
@@ -2,11 +2,5 @@ import cv2
 # IMREAD_GRAYSCALE
 img = cv2.imread('lenna.png', 0)
 cv2.imshow('lenna', img)
-k = cv2.waitKey(0)
-if k == 27: #esc key
-    cv2.destroyAllWindows()
-elif k == ord('s'): #'s' key
-    cv2.imwrite('lenna2.png', 1)
-    cv2.destroyAllWindows()
-
-
+cv2.waitKey(0)
+cv2.destroyAllWindows()
